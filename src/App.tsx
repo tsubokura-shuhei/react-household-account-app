46b3923d06c14a2c6439c25569cef23568263beb
tsubokura-shuhei <repeat2639@gmail.com>
2024-03-20 21:45:46 +0900

    chapter99 complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,202 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Report from "./pages/Report";
+import NoMatch from "./pages/NoMatch";
+import AppLayout from "./pages/components/layout/AppLayout";
+import { theme } from "./theme/theme";
+import { ThemeProvider } from "@emotion/react";
+import { CssBaseline, selectClasses } from "@mui/material";
+import { Transaction } from "./types/index";
+
+import {
+  collection,
+  getDocs,
+  addDoc,
+  doc,
+  deleteDoc,
+  updateDoc,
+} from "firebase/firestore";
+import { db } from "./firebase";
+import { format } from "date-fns";
+import { formatMonth } from "./utils/formatting";
+import { Schema } from "./validations/schema";
 
 function App() {
+  //取引データを管理
+  const [transaction, setTransaction] = useState<Transaction[]>([]);
+
+  // 今月のデータを管理するState
+  const [currentMonth, setCurrentMonth] = useState(new Date());
+
+  //ローディング管理
+  const [isLoading, setIsLoading] = useState(true);
+
+  //firebaseエラーかどうかを判定する型ガード
+  function isFireStoreError(
+    err: unknown
+  ): err is { code: string; message: string } {
+    return typeof err === "object" && err !== null && "code" in err;
+  }
+
+  useEffect(() => {
+    try {
+      //firebaseのfirestoreからデータを全てのデータを取得する
+      const allDate = async () => {
+        const querySnapshot = await getDocs(collection(db, "Transactions"));
+        const transactionsData = querySnapshot.docs.map((doc) => {
+          return {
+            ...doc.data(),
+            id: doc.id,
+          } as Transaction;
+        });
+        // console.log(transactionsData);
+        setTransaction(transactionsData);
+      };
+      allDate();
+    } catch (err) {
+      if (isFireStoreError(err)) {
+        console.error("Firebaseに関するエラーは:", err);
+        console.error("Firebaseのエラーメッセージは:", err.message);
+        console.error("Firebaseのエラーコードは:", err.code);
+      } else {
+        console.error("一般的なエラーは:", err);
+      }
+    } finally {
+      setIsLoading(false);
+    }
+  }, []);
+
+  //今月のデータを取得
+  const monthlyTransactions = transaction.filter((allDate) => {
+    return allDate.date.startsWith(formatMonth(currentMonth));
+  });
+
+  //取引を保存する処理
+  const handleSaveTransaction = async (transactionData: Schema) => {
+    try {
+      // firestoreにデータを保存
+      const docRef = await addDoc(collection(db, "Transactions"), {
+        ...transactionData,
+      });
+      // console.log("Document written with ID: ", docRef.id);
+
+      const newTransaction = {
+        id: docRef.id,
+        ...transactionData,
+      } as Transaction;
+      // console.log(newTransaction);
+      setTransaction((prevTransaction) => [...prevTransaction, newTransaction]);
+    } catch (err) {
+      if (isFireStoreError(err)) {
+        console.error("Firebaseに関するエラーは:", err);
+        console.error("Firebaseのエラーメッセージは:", err.message);
+        console.error("Firebaseのエラーコードは:", err.code);
+      } else {
+        console.error("一般的なエラーは:", err);
+      }
+    }
+  };
+
+  // console.log(monthlyTransactions);
+
+  const handleDeleteTransaction = async (
+    transactionIds: string | readonly string[]
+  ) => {
+    //firestoreのデータを削除
+    try {
+      //配列でデータを管理する
+      const idsToDelete = Array.isArray(transactionIds)
+        ? transactionIds
+        : [transactionIds];
+
+      for (const id of idsToDelete) {
+        await deleteDoc(doc(db, "Transactions", id));
+      }
+
+      //画面上にすぐに反映させる
+      // const filterdTransactions = transaction.filter(
+      //   (transactionDeleteDate) => transactionDeleteDate.id !== transactionId
+      // );
+      const filterdTransactions = transaction.filter(
+        (transactionDeleteDate) =>
+          !idsToDelete.includes(transactionDeleteDate.id)
+      );
+
+      setTransaction(filterdTransactions);
+    } catch (err) {
+      if (isFireStoreError(err)) {
+        console.error("Firebaseに関するエラーは:", err);
+        console.error("Firebaseのエラーメッセージは:", err.message);
+        console.error("Firebaseのエラーコードは:", err.code);
+      } else {
+        console.error("一般的なエラーは:", err);
+      }
+    }
+  };
+
+  const handleUpdateTransaction = async (
+    transactionUpdata: Schema,
+    transactionId: string
+  ) => {
+    try {
+      //firestore更新処理
+      const docRef = doc(db, "Transactions", transactionId);
+      await updateDoc(docRef, transactionUpdata);
+
+      //即反映の処理
+      const updatedTransactions = transaction.map((t) =>
+        t.id === transactionId ? { ...t, ...transactionUpdata } : t
+      ) as Transaction[];
+
+      setTransaction(updatedTransactions);
+    } catch (err) {
+      if (isFireStoreError(err)) {
+        console.error("Firebaseに関するエラーは:", err);
+        console.error("Firebaseのエラーメッセージは:", err.message);
+        console.error("Firebaseのエラーコードは:", err.code);
+      } else {
+        console.error("一般的なエラーは:", err);
+      }
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Router>
+        <Routes>
+          <Route path="/" element={<AppLayout />}>
+            <Route
+              index
+              element={
+                <Home
+                  monthlyTransactions={monthlyTransactions}
+                  setCurrentMonth={setCurrentMonth}
+                  onSaveTransaction={handleSaveTransaction}
+                  onDeleteTransaction={handleDeleteTransaction}
+                  onUpdateTransaction={handleUpdateTransaction}
+                />
+              }
+            />
+            <Route
+              path="/report"
+              element={
+                <Report
+                  currentMonth={currentMonth}
+                  setCurrentMonth={setCurrentMonth}
+                  monthlyTransactions={monthlyTransactions}
+                  isLoading={isLoading}
+                  onDeleteTransaction={handleDeleteTransaction}
+                />
+              }
+            />
+            <Route path="*" element={<NoMatch />} />
+          </Route>
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
 
---
+++ src/pages/components/BarChart.tsx
@@ -0,0 +1,101 @@
+import React from "react";
+import { Bar } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Transaction } from "../../types";
+import { calculateDailyBalances } from "../../utils/financeCalculations";
+import { Box, Typography, useTheme } from "@mui/material";
+import CircularProgress from "@mui/material/CircularProgress";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+interface BarChartProps {
+  monthlyTransactions: Transaction[];
+  isLoading: boolean;
+}
+
+const BarChart = ({ monthlyTransactions, isLoading }: BarChartProps) => {
+  const theme = useTheme();
+
+  // オプション設定
+  const options = {
+    maintainAspectRatio: false,
+    responsive: true,
+    plugins: {
+      // legend: {
+      //   position: "top" as const,
+      // },
+      title: {
+        display: true,
+        text: "日別の収支",
+      },
+    },
+  };
+
+  //日付ごとの収支の算出
+  const dailyBalances = calculateDailyBalances(monthlyTransactions);
+  // console.log(dailyBalances);
+  // console.log(dailyBalances);
+  // console.log(monthlyTransactions, "1ヶ月分の取引データ");
+
+  //ラベル（日付）を配列へ格納
+  const dateLabels = Object.keys(dailyBalances).sort();
+
+  //日付ごとの支出を配列へ格納
+  const expenseData = dateLabels.map((day) => dailyBalances[day].expense);
+
+  //日付ごとの収入を配列へ格納
+  const incomeData = dateLabels.map((day) => dailyBalances[day].income);
+
+  //データ設定
+  const data = {
+    labels: dateLabels,
+    datasets: [
+      {
+        label: "収入",
+        data: incomeData,
+        backgroundColor: theme.palette.incomeColor.light,
+      },
+      {
+        label: "支出",
+        data: expenseData,
+        backgroundColor: theme.palette.expenseColor.light,
+      },
+    ],
+  };
+
+  return (
+    <Box
+      sx={{
+        flexGrow: 1,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      {isLoading ? (
+        <CircularProgress />
+      ) : monthlyTransactions.length > 0 ? (
+        <Bar options={options} data={data} />
+      ) : (
+        <Typography>データがありません</Typography>
+      )}
+    </Box>
+  );
+};
+
+export default BarChart;
